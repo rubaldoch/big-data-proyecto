11cfb1b5529fda140a37cdeb7ce40819435a5698
Ariana Villegas <ariana.villegas@utec.edu.pe>
2022-06-21 11:12:14 -0500

    Update format

--- src/database.py
+++ src/database.py
@@ -10,26 +10,29 @@ def format_db(dict_json):
         ans.append([key, dict_json[key]])
     return ans
 
+
 def create_df(data):
     sc = SparkSession.builder.getOrCreate()
-    data_frame = [ ]
+    data_frame = []
     i = 1
     for secuence in data:
         for event in secuence:
             data_frame.append((i, event[0], event[1][0], event[1][1]))
         i += 1
-    
+
     columns = ['Sequence', 'Event', 'Start', 'End']
 
     df = sc.createDataFrame(data=data_frame, schema=columns)
     return df
 
+
 def create_database(df_data):
-    df_data = df_data.orderBy([ "Event", "Sequence", "Start", "End"],ascending = [ True, True,  True, True])
+    df_data = df_data.orderBy(["Event", "Sequence", "Start", "End"], ascending=[
+                              True, True,  True, True])
     df_collect = df_data.collect()
     element = ""
     for row in df_collect:
         if element != row["Event"]:
             element = row["Event"]
-        
+
     return df_data
